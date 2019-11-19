00294a65cb6e1ac1d619446a18cc6a248366a074
Andikan Gabriel <andikan@sure.ng>
2019-11-19 16:01:52 +0100

    Add package facade

---
+++ src/Facades/Coindesk.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace GabrielAndy\Coindesk\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Coindesk extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'coindesk';
+    }
+}
