5e67512aac85a3870b0c0621088f387b1190e8db
Andikan Gabriel <andikan@sure.ng>
2019-11-19 15:59:41 +0100

    Add package config file

---
+++ config/coindesk.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    /*
+     * The api url used when sending requests to Coindesk
+     *
+     * Coindesk have api versions 1 and 2. Choice of version is flexible.
+     * However, the default is version 1.
+     */
+    'apiUrl' => 'https://api.coindesk.com/v1/bpi/currentprice.json',
+];
