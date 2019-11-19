2fe0c29bde4dc477de1c345732517007dd397a26
Andikan Gabriel <andikan@sure.ng>
2019-11-19 16:06:35 +0100

    Add service provider

---
+++ src/CoindeskServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace GabrielAndy\Coindesk;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\ServiceProvider;
+
+class CoindeskServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/coindesk.php' => config_path('coindesk.php'),
+        ], 'config');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/coindesk.php', 'coindesk');
+
+        $this->app->bind('coindesk', function ($app) {
+            return (new Coindesk(new Client))
+                ->setApiURL(config('coindesk.apiUrl'));
+        });
+    }
+}
