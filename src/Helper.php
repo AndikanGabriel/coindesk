35e7d6ef686009c2d5260bee318e81a9b2ef60f8
Andikan Gabriel <andikan@sure.ng>
2019-11-19 16:07:07 +0100

    Update helper class

--- src/Helper.php
+++ src/Helper.php
@@ -25,4 +25,26 @@ class Helper
     {
         return self::isFiatCurrency($currencyCode) || self::isCryptoCurrency($currencyCode);
     }
+
+    /**
+     * Check for a fiat currency code.
+     *
+     * @param  string  $currencyCode
+     * @return boolean
+     */
+    public static function isFiatCurrency($currencyCode)
+    {
+        return in_array(strtoupper($currencyCode), CoindeskCurrenciesFormat::fiatCurrencies());
+    }
+
+    /**
+     * Check if currency code is supported by Coindesk.
+     *
+     * @param  string  $currencyCode
+     * @return boolean
+     */
+    public static function currencySupport($currencyCode, $finder)
+    {
+        return in_array(strtoupper($currencyCode), array_keys($finder));
+    }
 }
