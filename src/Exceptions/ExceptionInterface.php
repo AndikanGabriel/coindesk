d3ac93923dac206a398318e800d0493993218824
Andikan Gabriel <54823365+AndikanGabriel@users.noreply.github.com>
2021-05-09 16:13:43 +0100

    update exceptions
    add uncategorized exception - CoindeskException
    
    add exception interface
    
    Exception Interface ties all package exceptions to the
    `Throwable` interface for catching and logging intentions.
    
    add exception for http errors
    
    add exception for invalid currency code supplied
    
    add exception for unsupported currency code

---
+++ src/Exceptions/ExceptionInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace GabrielAndy\Coindesk\Exceptions;
+
+interface ExceptionInterface extends \Throwable
+{
+
+}
---
+++ src/Exceptions/UnsupportedCurrencyCode.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace GabrielAndy\Coindesk\Exceptions;
+
+class UnsupportedCurrencyCode extends \UnexpectedValueException implements ExceptionInterface
+{
+
+}
