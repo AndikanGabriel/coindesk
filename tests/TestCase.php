2c8deaed5e95debc9b5b1b3d762ae0a65096e613
Andikan Gabriel <54823365+AndikanGabriel@users.noreply.github.com>
2021-05-12 03:57:20 +0100

    add test suite
    add test suite configuration
    
    ignore tests cache and coverage report
    
    update CONTRIBUTING.md
    
    Add notes on the necessity of testing the package when needed.

---
+++ tests/TestCase.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace GabrielAndy\Coindesk\Tests;
+
+use Coindesk;
+use GabrielAndy\Coindesk\CoindeskFacade;
+use GabrielAndy\Coindesk\CoindeskServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+abstract class TestCase extends OrchestraTestCase
+{
+    /**
+     * Load package service provider.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            CoindeskServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Load package alias/facade.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            'Coindesk' => CoindeskFacade::class,
+        ];
+    }
+} 
